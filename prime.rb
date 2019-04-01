10799c52e0da61f6b386034179ba9e8722a692c6
Iskra B <ibatistapoblete@gmail.com>
2019-04-01 02:26:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,7 @@
 def prime?(n)
-  if n = 2
-    return true
-  elsif n <= 1
-    return false
-  elsif (2..Math.sqrt(n)).to_a.each do |x| n % x == 0 end
-    return false
-  else
-    return true
-  end
+  return false if !n.integer?
+  return false if n < 2
+  return true if n == 2
+  (2..n-1).each {|i| return false if n % i == 0}
+  true
 end
